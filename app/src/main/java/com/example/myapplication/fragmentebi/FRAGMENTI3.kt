0bd79244f7383d9b7b0519f3a9b4e21d9cfab56f
zazag <zaza.gokadze.1@btu.edu.ge>
2022-01-08 21:46:11 +0400

    Revert "davaleba 1 zaza"
    This reverts commit e3a0147f

--- app/src/main/java/com/example/myapplication/fragmentebi/FRAGMENTI3.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication.fragmentebi
-
-import androidx.fragment.app.Fragment
-import com.example.myapplication.R
-
-class FRAGMENTI3 : Fragment(R.layout.third_fragment) {
-}
--- app/src/main/java/com/example/myapplication/fragmentebi/FRAGMENTI4.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.fragmentebi
-
-import android.os.Parcel
-import android.os.Parcelable
-import androidx.fragment.app.Fragment
-import com.example.myapplication.R
-
-class FRAGMENTI4() : Fragment(R.layout.my_fragment){
-}
